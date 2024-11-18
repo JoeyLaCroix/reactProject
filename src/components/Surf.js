6dba1987c470a745e49d5575579cb76b4e723edf
joey LaCroix <JLACROIX@email.sc.edu>
2024-11-17 21:29:52 -0500

    new

--- src/components/Surf.js
+++ src/components/Surf.js
@@ -1,11 +1,25 @@
-import { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import SurfList from "./SurfList";
+import AddDialog from "../components/addDialog";
+import "../css/surfadd.css";
 
 
 const Surf = () => {
   const [surfers, setSurfers] = useState([]);
 
+  const addSurfer = (surfer) =>{
+    setSurfers ((surfer)=>[...surfers, surfer]);
+};
+const [showAddDialog, setShowAddDialog] = React.useState(false);
+
+const openDialog = () => {
+    setShowAddDialog(true);
+};
+const closeDialog = () => {
+    setShowAddDialog(false);
+};
+  
   useEffect(() => {
     (async () => {
       const response = await axios.get("https://projectbackend-qwro.onrender.com/api/surfers");
@@ -16,6 +30,13 @@ const Surf = () => {
   return (
     <div className="surfers-list">
       <div className="columns">
+      <h1 id="surftitle">Our Surfers:</h1>
+        <div id="button">
+        <button id="add-surfer" onClick = {openDialog}>Become a SSC representative</button>
+        
+        {showAddDialog ?(<AddDialog closeDialog={closeDialog} addSurfer={addSurfer}/>):("")}
+        </div>
+
         {surfers.map((surfList) => (
           <SurfList
             name={surfList.name}
--- src/pages/surfers.js
+++ src/pages/surfers.js
@@ -1,18 +1,22 @@
 import "../css/surfers.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import AddDialog from "../components/addDialog";
 import lanikai from "../images/lani-kai.jpg";
 import kanoa from "../images/kanoa.jpg";
 import SurfList from "../components/SurfList";
 import Surf from "../components/Surf";
+import "../css/surfadd.css";
+
+
 
 function Surfers() {
+
+
 return(
     <>
     <Header />
-    
-    <div className="Surfers">
-        <h1 id="surftitle">Our Surfers:</h1>
+    <div className="Surfers" id="add">
         <div id="main-surf-list">
         <Surf />
         </div>
