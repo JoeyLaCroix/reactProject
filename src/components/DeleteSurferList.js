1169c81e7f16cd895fb7832dea32dd6334d6349d
joey LaCroix <JLACROIX@email.sc.edu>
2024-11-29 19:59:11 -0500

    error

---
+++ src/components/DeleteSurferList.js
@@ -0,0 +1,46 @@
+
+import React, { useState } from "react";
+
+const DeleteSurferList = (props) => {
+  const [result, setResult] = useState("");
+
+  const deleteSurferList = async() => {
+   const response = await fetch(`https://projectbackend-qwro.onrender.com/${props._id}`,{
+    method:"DELETE"
+   });
+
+   if(response.status === 200){
+    setResult("Surfer Succesfully Deleted");
+    props.hideSurferList();
+    props.closeDialog();
+   } else {
+    setResult("Sorry, we couldn't delete your Surfer at this time.");
+   }
+  };
+
+  return (
+    <div id="delete-dialog" className="w3-modal">
+      <div className="w3-modal-content">
+        <div className="w3-container">
+          <span
+            id="dialog-close"
+            className="w3-button w3-display-topright"
+            onClick={props.closeDialog}
+          >
+            &times;
+          </span>
+          <div id="delete-content">
+            <h3>Are you sure you want to delete the {props.name}</h3>
+            <section>
+              <button onClick={props.closeDialog}>No</button>            
+              <button onClick={deleteSurferList}>Yes</button>
+            </section>
+            <span>{result}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteSurferList;
--- src/components/SurfList.js
+++ src/components/SurfList.js
@@ -1,12 +1,75 @@
-const SurfList = (surfList) => {
+import "../css/surflist.css";
+import React, { useState } from "react";
+import EditHousePlan from "./EditSurferList";
+import DeleteSurferList from "./DeleteSurferList";
+
+
+const SurfList = (props) => {
+    const [showEditDialog, setShowEditDialog] = useState(false);
+    const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+    const [SurfList, setSurfList] = useState(props);
+    const [showSurferList, setSurferList] = useState(true);
+  
+    const openEditDialog = () => {
+      setShowEditDialog(true);
+    };
+  
+    const closeEditDialog = () => {
+      setShowEditDialog(false);
+    };
+  
+    const openDeleteDialog = () => {
+      setShowDeleteDialog(true);
+    };
+  
+    const closeDeleteDialog = () => {
+      setShowDeleteDialog(false);
+    };
+  
+    const hideSurferList = () => {
+      setSurferList(false);
+    }
+  
+    const editSurferList = (newSurferList) => {
+      setSurferList(newSurferList);
+    };  
+  
     return (
+<>
+        {showSurferList ? (
+        <div>
+          {showDeleteDialog ? (
+            <DeleteSurferList
+              closeDialog={closeDeleteDialog}
+              hideHousePlan = {hideSurferList}
+              name={SurfList.name}
+              id={SurfList._id}
+            />
+          ) : (
+            ""
+          )}
+
         <section className="surfers">
-            <h3>Name:{surfList.name}</h3>
-            <h4>Hometown:{surfList.hometown}</h4>
-            <h4>SurfType:{surfList.surftype}</h4>
-            <p><img src={"https://projectbackend-qwro.onrender.com/images/"+surfList.image} alt={surfList.name} /></p>
-            <p>Bio:{surfList.bio}</p>
+            <section id="change-buttons">
+                  <a href="#" >
+                    &#9998; 
+                  </a>
+                  
+                  <a href="#" onClick={openDeleteDialog}>
+                    &#x2715; 
+                  </a>
+            </section>
+            <h3>Name:{props.name}</h3>
+            <h4>Hometown:{props.hometown}</h4>
+            <h4>SurfType:{props.surftype}</h4>
+            <p><img src={"https://projectbackend-qwro.onrender.com/images/"+props.image} alt={props.name} /></p>
+            <p>Bio:{props.bio}</p>
         </section>
+            </div>
+          ) : (
+            ""
+          )}
+        </>
     );
 };
 
